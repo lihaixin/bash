451313783eca22ec41a8ecc5ac90e5c8a03b0aa9
lihaixin <lihaixin@15099.net>
2022-01-28 20:26:36 +0800

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -3,7 +3,7 @@
 # curl -sSL https://tinyurl.com/proxmoxwifi | bash -x
 
 apt install wpasupplicant parprouted iw net-tools wireless-tools iperf3
-wpa_passphrase 406_5G wzTZ33406 > /etc/wpa_supplicant/wpa_supplicant.conf
+wpa_passphrase $1 $2 > /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo 'auto lo
 iface lo inet loopback
@@ -17,11 +17,11 @@ iface lo inet loopback
 #        bridge-stp off
 #        bridge-fd 0
 
-#add wifi interface
+#add wifi interface for intel
 allow-hotplug wlxc8e7d8cbf183
 auto wlxc8e7d8cbf183
 ifacewlxc8e7d8cbf183 inet static
-        address 192.168.2.207/24
+        address 192.168.2.107/24
         gateway 192.168.2.254
         post-up wpa_supplicant -B -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlxc8e7d8cbf183
         post-up /usr/sbin/parprouted vmbr0 wlxc8e7d8cbf183
@@ -30,5 +30,27 @@ ifacewlxc8e7d8cbf183 inet static
 auto vmbr0
 iface vmbr0 inet static
       bridge_ports none
-      address 192.168.2.207/24
+      address 192.168.2.107/24
+# wifi bridge' > /etc/network/interfaces
+
+echo 'auto lo
+iface lo inet loopback
+
+#iface ens18 inet manual
+
+#auto vmbr0
+#iface vmbr0 inet static
+#        address 192.168.2.107/24
+#        gateway 192.168.2.254
+#        bridge-ports ens18
+#        bridge-stp off
+#        bridge-fd 0
+
+
+auto vmbr0
+iface vmbr0 inet static
+      bridge_ports none
+      address 192.168.2.107/24
+      post-up /usr/sbin/parprouted vmbr0 wlxc8e7d8cbf183
+      post-down /usr/bin/killall /usr/sbin/parprouted
 # wifi bridge' > /etc/network/interfaces
