b380b15c6718d044eb04ad6ba1be8557250d736c
lihaixin <lihaixin@15099.net>
2022-01-29 15:23:50 +0800

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -21,6 +21,7 @@ chmod u-w /etc/sudoers
 # APT::Install-Suggests "0";
 # task-xfce-desktop task-cinnamon-desktop task-laptop
 # apt -y install --no-install-recommends task-cinnamon-desktop cinnamon-l10n
+# 使用完整的桌面环境，网卡可以自动开机联网
 apt -y install task-cinnamon-desktop
 # 
 apt-get -y install task-chinese-s task-chinese-s-desktop
