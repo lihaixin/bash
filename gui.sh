6c7193c7f29ae67a18adb2f22984ab8d6aa9f5bc
lihaixin <lihaixin@15099.net>
2022-01-29 11:11:21 +0800

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -19,8 +19,8 @@ chmod u-w /etc/sudoers
 # 默认安装推荐包，不安装建议建议包，迷你安装ba推荐包也不安装
 # APT::Install-Recommends "1"; --no-install-recommends
 # APT::Install-Suggests "0";
-# apt -y install --no-install-recommends task-xfce-desktop
-apt-get -y install task-cinnamon-desktop
+apt -y install --no-install-recommends task-xfce-desktop
+# apt-get -y install task-cinnamon-desktop
 apt-get -y install task-chinese-s task-chinese-s-desktop
 # apt-get -y install task-laptop
 
