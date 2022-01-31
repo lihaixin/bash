4f4434b75b9468d97beefba2292ddb5a0c73dc06
lihaixin <lihaixin@15099.net>
2022-01-31 11:27:46 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# curl -sSL https://tinyurl.com/proxmoxdocker | bash -x
-# wget -qO - https://tinyurl.com/proxmoxdocker | bash -x
+# curl -sSL https://15099.net/proxmox.docker | bash -x
+# wget -qO - https://15099.net/proxmox.docker | bash -x
 
 ## 换成国内源
 cat >/etc/apt/sources.list << TEMPEOF
