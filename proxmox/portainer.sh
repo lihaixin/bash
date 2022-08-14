bd9191202b10663c75339b39933aa9c66741690f
lihaixin <lihaixin@15099.net>
2022-08-14 09:46:42 +0800

    Update portainer.sh

--- proxmox/portainer.sh
+++ proxmox/portainer.sh
@@ -8,17 +8,19 @@ docker volume create portainer_data
 
 ## 创建容器
 
-docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer \
+docker stop portainer2 && docker rm portainer2
+
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer2 \
     --restart=always \
     --label owner=portainer \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    portainer/portainer-ce \
+    portainer/portainer-ce:2.13.0-alpine \
     -l owner=portainer \
     --templates https://git.io/portainer \
     --logo https://git.io/docker.logo
 
-
+2.14.0-alpine
 ## 添加自动更新
 docker run -d \
     --name watchtower \
