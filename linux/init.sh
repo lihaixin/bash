c6c4928dcc15cf7b15ba10ac64a83f3d13ef62e8
lihaixin <lihaixin@15099.net>
2024-08-29 23:22:43 +0800

    Create init.sh

---
+++ linux/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DEFAULT_VALUE="默认值"
+prompt="请输入内容，10秒内无输入将采用默认值: "
+
+# 使用read的-t选项及命令替换特性
+read -t 10 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
+echo "${USER_INPUT:-$DEFAULT_VALUE}"
+
+#后续操作基于${USER_INPUT:-$DEFAULT_VALUE}进行
+bash
