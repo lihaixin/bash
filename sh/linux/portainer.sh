a3e145865bc6a3703f1faa04cbea75ac4b493c1a
lihaixin <lihaixin@15099.net>
2024-09-01 00:16:20 +0800

    Create portainer.sh

---
+++ sh/linux/portainer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "准备安装Portainer 图像界面..."
+
+#######################################################install_portainer########################################################################################
+install_docker() {
+echo "开始安装Portainer 图像界面"
+DEFAULT_VALUE="@china1234567"
+prompt="请输入admin密码，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
+# 使用read的-t选项及命令替换特性
+read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
+: ${USER_INPUT:$DEFAULT_VALUE}"
+
+docker stop ui
+docker rm ui
+docker pull lihaixin/portainer:ce-2.19.5
+docker run -d \
+-p 9443:9443 \
+-p 8000:8000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v portainer_data:/data \
+-e TPORT=8000 \
+-e COUNTRY=cn \
+-e PASSWORD="$USER_INPUT" \
+--name ui \
+--restart=always \
+lihaixin/portainer:ce-2.19.5
+}
+install_docker
