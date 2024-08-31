c2a790d90e6b76ab46c43d31161bed9b3af15643
lihaixin <lihaixin@15099.net>
2024-08-31 17:31:31 +0800

    Create online_install_linux.sh

---
+++ sh/linux/online_install_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "准备在线安装Debian 11 纯系统"
+
+DEFAULT_VALUE="password"
+prompt="请输入安装后密码，10秒内无输入将采用默认值(password): "
+read -t 10 -p "$prompt" PASSWORD || PASSWORD=$DEFAULT_VALUE
+: ${PASSWORD:=$DEFAULT_VALUE}
+
+if [ "$COUNTRY" == "cn" ]; then
+  bash <(wget --no-check-certificate -qO- 'https://bash.15099.net/linux/netinstallos.sh') -d 11 -v 64 -p "$PASSWORD" --mirror 'http://mirrors.aliyun.com/debian/'
+else
+  bash <(wget --no-check-certificate -qO- 'https://bash.15099.net/linux/netinstallos.sh') -d 11 -v 64 -p "$PASSWORD"
+fi
