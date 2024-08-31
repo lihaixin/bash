e1812585db39affc8eb9e238754bfb912a43f6b6
lihaixin <lihaixin@15099.net>
2024-09-01 00:10:49 +0800

    Update docker.sh

--- sh/linux/docker.sh
+++ sh/linux/docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "准备初始化docker"
+echo "准备初始化docker..."
 
 #######################################################chang_repo_update########################################################################################
 chang_repo() {
-echo "调整仓库信息、系统升级、安装基本包"
+echo "开始安装doceker.io"
 DEFAULT_VALUE="Y"
 prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
@@ -11,49 +11,34 @@ read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:$DEFAULT_VALUE}"
 
 if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
-    echo "现在调整仓库信息、系统升级、安装基本包"
-    case $OS in
-        debian)
-            echo "执行针对Debian的操作..."
-            sed -i.bak 's#http://deb.debian.org#https://mirrors.tuna.tsinghua.edu.cn/debian#g' /etc/apt/sources.list
-            apt update -y && apt upgrade -y
-            apt-get install net-tools wget ntpdate -y
-            ;;
-        ubuntu)
-            echo "执行针对Ubuntu的操作..."
-            sed -i.bak 's#http://archive.ubuntu.com#https://mirrors.tuna.tsinghua.edu.cn#g' /etc/apt/sources.list.d/sources.list
-            apt update -y && apt upgrade -y
-            apt-get install net-tools wget ntpdate -y
-            ;;
-        armbian)
-            echo "执行针对Armbian的操作..."
-            sed -i.bak 's#http://apt.armbian.com#https://mirrors.tuna.tsinghua.edu.cn/armbian#g' /etc/apt/sources.list.d/armbian.list
-            sed -i.bak 's#http://archive.ubuntu.com#https://mirrors.tuna.tsinghua.edu.cn#g' /etc/apt/sources.list
-            apt update -y && apt upgrade -y
-            apt-get install net-tools wget ntpdate -y
-            ;;
-        *)
-            echo "未知的系统类型: $OS"
-            ;;
-    esac
+    echo "现在安装docker.io"
+    apt install docker.io
+    echo "成功安装docker.io"
+    mkdir -p /etc/docker 
+    tee /etc/docker/daemon.json <<-'EOF'
+    	{
+    	  "log-driver": "json-file", 
+    	  "log-opts": { 
+    	  "max-size": "20m", "max-file": "3" 
+    	  },
+          "dns" : [
+            "8.8.8.8"
+          ]
+    	}
+    EOF
 else
-    echo "不调正仓库，直接升级,安装基本包"
-    case $OS in
-        debian)
-            echo "执行针对Debian的操作..."
-            apt update -y && apt upgrade -y
-            ;;
-        ubuntu)
-            echo "执行针对Ubuntu的操作..."
-            apt update -y && apt upgrade -y
-            ;;
-        armbian)
-            echo "执行针对Armbian的操作..."
-            apt update -y && apt upgrade -y
-            ;;
-        *)
-            echo "未知的系统类型: $OS"
-            ;;
-    esac
+    echo "现在安装docker.io"
+    apt install docker.io
+    echo "成功安装docker.io"
+    mkdir -p /etc/docker 
+    tee /etc/docker/daemon.json <<-'EOF'
+    	{
+    	  "registry-mirrors": ["https://hub.15099.net"],
+    	  "log-driver": "json-file", 
+    	  "log-opts": { 
+    	  "max-size": "20m", "max-file": "3" 
+    	  }
+    	}
+    EOF
 fi
 }
