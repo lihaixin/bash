c57f8d1ab6b7122329d639de1b931d686aa149c1
lihaixin <lihaixin@15099.net>
2024-09-01 18:04:49 +0800

    Update portainer.sh

--- sh/linux/portainer.sh
+++ sh/linux/portainer.sh
@@ -8,7 +8,7 @@ DEFAULT_VALUE="@china1234567"
 prompt="请输入admin密码，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:$DEFAULT_VALUE}
+: ${USER_INPUT:=$DEFAULT_VALUE}
 docker stop ui
 docker rm ui
 docker pull lihaixin/portainer:ce-2.19.5
