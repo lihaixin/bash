795f40eb969cc8c1280e15d0941fb38549ae7234
lihaixin <lihaixin@15099.net>
2024-09-01 11:13:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc /usr/share/nginx/html/README.md -s --metadata charset=utf-8 --metadata title="bash" -o /usr/share/nginx/html/index.html
+pandoc /usr/share/nginx/html/README.md -s --metadata charset=utf-8 --metadata title="个人脚本管理系统" -o /usr/share/nginx/html/index.html
 nginx -g "daemon off;"
