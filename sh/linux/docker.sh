a44813bbed3ed71ff058dd7ed04929ac1e99f460
lihaixin <lihaixin@15099.net>
2024-09-01 08:26:04 +0800

    Update docker.sh

--- sh/linux/docker.sh
+++ sh/linux/docker.sh
@@ -3,11 +3,11 @@ echo "准备初始化docker..."
 
 #######################################################install_docker########################################################################################
 install_docker() {
-echo "开始安装doceker-io"
+echo "准备安装doceker-io"
 # debian10/11 ver: 20.10 docker.io
 # armbian 24.5 ver: 20.10 docker.io
 DEFAULT_VALUE="Y"
-prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
+prompt="请输入内容(Y/N)确定是否现在安装docker-ce，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:$DEFAULT_VALUE}"
