973ec5482d36e84cc06d97edecfd38f3df296cdb
lihaixin <lihaixin@15099.net>
2024-09-01 18:30:17 +0800

    Update init.sh

--- sh/linux/init.sh
+++ sh/linux/init.sh
@@ -8,7 +8,7 @@ DEFAULT_VALUE="Y"
 prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:$DEFAULT_VALUE}"
+: ${USER_INPUT:=$DEFAULT_VALUE}"
 
 if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
     echo "现在调整仓库信息、系统升级、安装基本包"
@@ -66,7 +66,7 @@ DEFAULT_VALUE="Y"
 prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:$DEFAULT_VALUE}"
+: ${USER_INPUT:=$DEFAULT_VALUE}"
 if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
     echo "现在调整DNS为国内DNS解析"
     #国内DNS
@@ -97,7 +97,7 @@ DEFAULT_VALUE="qq_hk20_000_gost"
 prompt="请输入内容(厂家_国别+编号_用户编号_用途 | qq_hk20_000_gost)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:$DEFAULT_VALUE}"
+: ${USER_INPUT:=$DEFAULT_VALUE}"
 HNAME=$USER_INPUT
 echo $HNAME > /etc/hostname
 hostname $HNAME
@@ -110,7 +110,7 @@ DEFAULT_VALUE="!passWord"
 prompt="请输入root密码)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:$DEFAULT_VALUE}"
+: ${USER_INPUT:=$DEFAULT_VALUE}"
 echo -e "$USER_INPUT\n$USER_INPUT" | (passwd root) 
 
 echo "关闭X11转发和修改端口"
@@ -127,11 +127,11 @@ chang_ssh
 #######################################################chang_wireguard########################################################################################
 chang_wireguard() {
 echo "调整仓库安装Wireguard内核支持"
-DEFAULT_VALUE="Y"
+DEFAULT_VALUE="N"
 prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:$DEFAULT_VALUE}"
+: ${USER_INPUT:=$DEFAULT_VALUE}"
 
 if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
     echo "现在调整仓库信息、系统升级、安装基本包"
