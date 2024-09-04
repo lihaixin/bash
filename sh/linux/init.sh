7d3d7cc81e18a1a65352140dad8428cfb57dc427
lihaixin <lihaixin@15099.net>
2024-09-04 21:44:44 +0800

    Update init.sh

--- sh/linux/init.sh
+++ sh/linux/init.sh
@@ -8,7 +8,7 @@ DEFAULT_VALUE="Y"
 prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
 # 使用read的-t选项及命令替换特性
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
-: ${USER_INPUT:=$DEFAULT_VALUE}"
+: ${USER_INPUT:=$DEFAULT_VALUE}
 
 if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
     echo "现在调整仓库信息、系统升级、安装基本包"
