d9ba04cbff0678c87111802f991d7e71022e95e4
lihaixin <lihaixin@15099.net>
2024-09-25 10:31:58 +0800

    Update portainer.sh

--- sh/linux/portainer.sh
+++ sh/linux/portainer.sh
@@ -47,7 +47,7 @@ docker run -d \
 -p 9443:9443 \
 -p 8000:8000 \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v portainer_data:/data \
+-v /var/portainer_data:/data \
 -e TPORT=8000 \
 -e COUNTRY=cn \
 -e PASSWORD=${USER_INPUT} \
