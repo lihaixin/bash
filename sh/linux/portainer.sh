fbdd66f1cc3085680c01700f1e0843790b54a094
lihaixin <lihaixin@15099.net>
2024-09-25 10:05:03 +0800

    Update portainer.sh

--- sh/linux/portainer.sh
+++ sh/linux/portainer.sh
@@ -18,9 +18,14 @@ echo ""
 echo "开始安装Portainer 图像界面"
 DEFAULT_PASSWD="@china1234567"
 prompt="请输入admin密码，长度不低于12位，20秒内无输入将采用默认值( $DEFAULT_PASSWD ): "
-# 使用read的-t选项及命令替换特性
-read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_PASSWD
-: ${USER_INPUT:=$DEFAULT_PASSWD}
+read -t 20 -p "$prompt" USER_PASSWD || USER_PASSWD=$DEFAULT_PASSWD
+: ${USER_PASSWD:=$DEFAULT_PASSWD}
+
+DEFAULT_TEMPLATES="https://dockerfile.15099.net/index.json"
+prompt="请输入你自动的模板地址，40秒内无输入将采用默认值( $DEFAULT_TEMPLATES ): "
+read -t 40 -p "$prompt" USER_TEMPLATES || USER_TEMPLATES=$DEFAULT_TEMPLATES
+: ${USER_TEMPLATES:=$DEFAULT_TEMPLATES}
+
 docker stop ui 1> /dev/null 2>&1
 docker rm ui 1> /dev/null 2>&1
 docker pull lihaixin/portainer:ce-2.19.5
@@ -32,9 +37,10 @@ docker run -d \
 -e TPORT=8000 \
 -e COUNTRY=cn \
 -e PASSWORD=${USER_INPUT} \
+-e TEMPLATES=${USER_TEMPLATES} \
 --name ui \
 --restart=always \
 lihaixin/portainer:ce-2.19.5
-echo "Portainer成功安装，访问：https://${WANIP}:9443 账号：admin 密码：${USER_INPUT} "
+echo "Portainer成功安装，访问：https://${WANIP}:9443 账号：admin 密码：${USER_PASSWD} "
 }
 install_portainer
