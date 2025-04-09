d49c1b2648c1ce1a903aa16ce3bb9754e7ff8882
lihaixin <lihaixin@15099.net>
2025-04-09 13:59:41 +0800

    Update online_install_ikuai.sh

--- sh/linux/online_install_ikuai.sh
+++ sh/linux/online_install_ikuai.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
-echo "准备在线灌ikuai路由系统"
-echo "当前主机内存大小(MB): $MEM_TOTAL"
+echo "Preparing to install iKuai router system online"
+echo "Current host memory size (MB): $MEM_TOTAL"
 if [ "$MEM_TOTAL" -lt 2024 ]; then
-    echo "当前主机内存小于2G,爱快系统架构选择x32"
+    echo "Current host memory is less than 2G, choosing x32 architecture for iKuai system"
     wget https://www.ikuai8.com/download.php?n=/3.x/iso/iKuai8_x32_3.6.13_Build202301131532.iso -O /tmp/ikuai8.iso
 else
-    echo "当前主机内存大于2G，爱快系统架构选择x64"
+    echo "Current host memory is greater than 2G, choosing x64 architecture for iKuai system"
     wget https://www.ikuai8.com/download.php?n=/3.x/iso/iKuai8_x64_3.6.13_Build202301131532.iso -O /tmp/ikuai8.iso
 fi
 
-
-echo "挂载磁盘"
+echo "Mounting disk"
 mount -o loop /tmp/ikuai8.iso /mnt
-echo "复制文件到根目录"
+echo "Copying files to root directory"
 cp -rpf /mnt/boot /
 
-echo "准备重启，然后在腾讯云&阿里云的等云主机操作页面打开VNC界面"
+echo "Preparing to reboot, then open the VNC interface on the cloud host operation page of Tencent Cloud & Alibaba Cloud"
 DEFAULT_VALUE="N"
-prompt="请输入内容Y/N，10秒内无输入将采用默认值(N): "
+prompt="Please enter Y/N, if no input within 10 seconds, the default value (N) will be used: "
 
-# 使用read的-t选项及命令替换特性
+# Use the -t option of read and command substitution feature
 read -t 10 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:$DEFAULT_VALUE}
 
 if [ "$USER_INPUT" = "Y" ]; then
-    echo "正在重启系统..."
+    echo "Rebooting system..."
     reboot
 elif [ "$USER_INPUT" = "N" ]; then
-    echo "返回主菜单..."
-    # 这里假设main_menu是一个可执行脚本或命令
+    echo "Returning to main menu..."
+    # Assuming main_menu is an executable script or command
     sub_main_menu
 else
-    echo "无效的输入，请输入 Y 或 N"
+    echo "Invalid input, please enter Y or N"
 fi
