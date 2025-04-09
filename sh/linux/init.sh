36e6868d8a00a6144ce6116187ddc0fb5897bf03
lihaixin <lihaixin@15099.net>
2025-04-09 13:54:42 +0800

    Update init.sh

--- sh/linux/init.sh
+++ sh/linux/init.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "准备初始化linux"
+echo "Preparing to initialize Linux"
 
 #######################################################chang_repo_update########################################################################################
 chang_repo() {
 echo ""
-echo "调整仓库信息、系统升级、安装基本包"
+echo "Adjusting repository information, system upgrade, installing basic packages"
 DEFAULT_VALUE="Y"
-prompt="请输入内容(Y/N)，10秒内无输入将采用默认值( $DEFAULT_VALUE ): "
-# 使用read的-t选项及命令替换特性
+prompt="Enter content (Y/N), default value will be used if no input within 10 seconds ( $DEFAULT_VALUE ): "
+# Use read's -t option and command substitution feature
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:=$DEFAULT_VALUE}
 
-if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
-    echo "现在调整仓库信息、系统升级、安装基本包"
+if [ "$USER_INPUT" = "Y" ] && [ "$COUNTRY" = "cn" ]; then
+    echo "Now adjusting repository information, system upgrade, installing basic packages"
     case $OS in
         debian)
-            echo "执行针对Debian的操作..."
+            echo "Executing operations for Debian..."
             # https://mirrors.tuna.tsinghua.edu.cn/help/debian/
             sed -i.bak 's#http://deb.debian.org#https://mirrors.tuna.tsinghua.edu.cn/debian#g' /etc/apt/sources.list
             sed -i 's#http://security.debian.org#https://mirrors.tuna.tsinghua.edu.cn#g' /etc/apt/sources.list
@@ -23,13 +23,13 @@ if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
             apt-get install net-tools wget ntpdate xz-utils -y
             ;;
         ubuntu)
-            echo "执行针对Ubuntu的操作..."
+            echo "Executing operations for Ubuntu..."
             sed -i.bak 's#http://archive.ubuntu.com#https://mirrors.tuna.tsinghua.edu.cn#g' /etc/apt/sources.list.d/sources.list
             apt update -y && apt upgrade -y
             apt-get install net-tools wget ntpdate xz-utils -y
             ;;
         armbian)
-            echo "执行针对Armbian的操作..."
+            echo "Executing operations for Armbian..."
             #systemctl enable armbian-resize-filesystem
             #systemctl start armbian-resize-filesystem
             sed -i.bak 's#http://apt.armbian.com#https://mirrors.tuna.tsinghua.edu.cn/armbian#g' /etc/apt/sources.list.d/armbian.list
@@ -40,61 +40,61 @@ if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
             systemctl enable --now ssh.service
             ;;
         alpine)
-            echo "执行针对alpine的操作..."
-            # Alpine 默认是没有启用tun功能的，所以需要自行开启
+            echo "Executing operations for Alpine..."
+            # Alpine does not enable tun by default, so it needs to be enabled manually
             modprobe tun
             echo "tun" >>/etc/modules
 
-            # 检查是否生效
+            # Check if it takes effect
             lsmod | grep tun
             apk upgrade --no-cache --available
-            # 安装qemu-guest-agent tzdata
-            # 添加到默认启动
-            # 要检查 Alpine Linux 上启用的系统启动服务
+            # Install qemu-guest-agent tzdata
+            # Add to default startup
+            # Check enabled system startup services on Alpine Linux
             apk add qemu-guest-agent tzdata
             rc-update add qemu-guest-agent
             rc-status -a
             ;;
         *)
-            echo "未知的系统类型: $OS"
+            echo "Unknown system type: $OS"
             ;;
     esac
 else
-    echo "不调正仓库，直接升级,安装基本包"
+    echo "Not adjusting repository, directly upgrading and installing basic packages"
     case $OS in
         debian)
-            echo "执行针对Debian的操作..."
+            echo "Executing operations for Debian..."
             apt update -y && apt upgrade -y
             apt-get install net-tools wget ntpdate ntp xz-utils -y
             ;;
         ubuntu)
-            echo "执行针对Ubuntu的操作..."
+            echo "Executing operations for Ubuntu..."
             apt update -y && apt upgrade -y
             apt-get install net-tools wget ntpdate ntp xz-utils -y
             ;;
         armbian)
-            echo "执行针对Armbian的操作..."
+            echo "Executing operations for Armbian..."
             apt update -y && apt upgrade -y
             apt-get install net-tools wget ntpdate ntp xz-utils -y
             ;;
         alpine)
-            echo "执行针对alpine的操作..."
-            # Alpine 默认是没有启用tun功能的，所以需要自行开启
+            echo "Executing operations for Alpine..."
+            # Alpine does not enable tun by default, so it needs to be enabled manually
             modprobe tun
             echo "tun" >>/etc/modules
 
-            # 检查是否生效
+            # Check if it takes effect
             lsmod | grep tun
             apk upgrade --no-cache --available
-            # 安装qemu-guest-agent tzdata
-            # 添加到默认启动
-            # 要检查 Alpine Linux 上启用的系统启动服务
+            # Install qemu-guest-agent tzdata
+            # Add to default startup
+            # Check enabled system startup services on Alpine Linux
             apk add qemu-guest-agent tzdata
             rc-update add qemu-guest-agent
             rc-status -a
             ;;
         *)
-            echo "未知的系统类型: $OS"
+            echo "Unknown system type: $OS"
             ;;
     esac
 fi
@@ -104,15 +104,15 @@ chang_repo
 #######################################################chang_dns_time########################################################################################
 chang_dns_time() {
 echo ""
-echo "调整系统DNS和时区和时间"
+echo "Adjusting system DNS and timezone"
 DEFAULT_VALUE="Y"
-prompt="请输入内容(Y/N)，10秒内无输入将采用默认值( $DEFAULT_VALUE ): "
-# 使用read的-t选项及命令替换特性
+prompt="Enter content (Y/N), default value will be used if no input within 10 seconds ( $DEFAULT_VALUE ): "
+# Use read's -t option and command substitution feature
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:=$DEFAULT_VALUE}
-if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
-    echo "现在调整DNS为国内DNS解析"
-    #国内DNS
+if [ "$USER_INPUT" = "Y" ] && [ "$COUNTRY" = "cn" ]; then
+    echo "Now adjusting DNS to domestic DNS resolution"
+    # Domestic DNS
     rm -rf /etc/resolv.conf 
 cat > /etc/resolv.conf << EOF 
 nameserver 223.5.5.5 
@@ -121,8 +121,8 @@ EOF
     cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
     ntpdate cn.pool.ntp.org
 else
-    echo "现在调整DNS为国外DNS解析和时区"
-    #国外DNS
+    echo "Now adjusting DNS to foreign DNS resolution and timezone"
+    # Foreign DNS
     rm -rf /etc/resolv.conf 
 cat > /etc/resolv.conf << EOF 
 nameserver 8.8.8.8 
@@ -136,10 +136,10 @@ chang_dns_time
 #######################################################chang_hostname########################################################################################
 chang_hostname() {
 echo ""
-echo "调整系统主机名"
+echo "Adjusting system hostname"
 DEFAULT_VALUE="qq-hk20-000-gost"
-prompt="请输入内容(厂家-国别+编号-用户编号-用途 | qq-hk20-000-gost)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
-# 使用read的-t选项及命令替换特性
+prompt="Enter content (manufacturer-country+number-user number-purpose | qq-hk20-000-gost), default value will be used if no input within 20 seconds ( $DEFAULT_VALUE ): "
+# Use read's -t option and command substitution feature
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:=$DEFAULT_VALUE}
 HNAME=$USER_INPUT
@@ -150,34 +150,34 @@ chang_hostname
 #######################################################chang_ssh########################################################################################
 chang_ssh() {
 echo ""
-echo "调整系统SSH配置信息"
+echo "Adjusting system SSH configuration"
 DEFAULT_VALUE="N"
-prompt="请输入内容(Y/N)，10秒内无输入将采用默认值( $DEFAULT_VALUE ): "
-# 使用read的-t选项及命令替换特性
+prompt="Enter content (Y/N), default value will be used if no input within 10 seconds ( $DEFAULT_VALUE ): "
+# Use read's -t option and command substitution feature
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:=$DEFAULT_VALUE}
 if [ "$USER_INPUT" = "Y" ]; then
 DEFAULT_PASSWD="!passWord"
-prompt="请输入root密码)，10秒内无输入将采用默认值( $DEFAULT_PASSWD ): "
-# 使用read的-t选项及命令替换特性
+prompt="Enter root password, default value will be used if no input within 10 seconds ( $DEFAULT_PASSWD ): "
+# Use read's -t option and command substitution feature
 read -t 20 -p "$prompt" PASSWD_INPUT || PASSWD_INPUT=$DEFAULT_PASSWD
 : ${PASSWD_INPUT:=$DEFAULT_PASSWD}
 echo -e "$PASSWD_INPUT\n$PASSWD_INPUT" | (passwd root) 
 
-echo "生成本地SSH授权登录"
+echo "Generating local SSH authorized login"
 ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
 cd .ssh
 cp id_rsa.pub authorized_keys
 chmod 600 authorized_keys
 
-echo "关闭X11转发和修改端口"
+echo "Disabling X11 forwarding and changing port"
 sed -i s'/^X11Forwarding yes$/#X11Forwarding yes/' /etc/ssh/sshd_config 
 sed -i s'/^#Port 22$/Port 32123/' /etc/ssh/sshd_config 
 sed -i s'/^Port 22$/Port 32123/' /etc/ssh/sshd_config 
 sed -i s'/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config 
 sed -i s'/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service ssh restart
-echo "成功调整SSH端口为32123，密码为：$PASSWD_INPUT 开启主机密码登录"
+echo "Successfully changed SSH port to 32123, password is: $PASSWD_INPUT, enabled host password login"
 fi
 }
 chang_ssh
@@ -185,18 +185,18 @@ chang_ssh
 #######################################################chang_wireguard########################################################################################
 chang_wireguard() {
 echo ""
-echo "调整仓库安装Wireguard内核支持"
+echo "Adjusting repository to install Wireguard kernel support"
 DEFAULT_VALUE="N"
-prompt="请输入内容(Y/N)，20秒内无输入将采用默认值( $DEFAULT_VALUE ): "
-# 使用read的-t选项及命令替换特性
+prompt="Enter content (Y/N), default value will be used if no input within 20 seconds ( $DEFAULT_VALUE ): "
+# Use read's -t option and command substitution feature
 read -t 20 -p "$prompt" USER_INPUT || USER_INPUT=$DEFAULT_VALUE
 : ${USER_INPUT:=$DEFAULT_VALUE}
 
-if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
-    echo "现在调整仓库信息、系统升级、安装基本包"
+if [ "$USER_INPUT" = "Y" ] && [ "$COUNTRY" = "cn" ]; then
+    echo "Now adjusting repository information, system upgrade, installing basic packages"
     case $OS in
         debian)
-            echo "执行针对Debian的操作..."
+            echo "Executing operations for Debian..."
             echo 'deb http://mirrors.aliyun.com/debian buster-backports main' | tee /etc/apt/sources.list.d/buster-backports.list
             apt update -y
             apt install linux-headers-$(uname -r) -y
@@ -204,20 +204,20 @@ if [ "$USER_INPUT" = "Y" ] &&[ "$COUNTRY" = "cn" ]; then
             modprobe wireguard && lsmod | grep wireguard
             ;;
         ubuntu)
-            echo "执行针对Ubuntu的操作..."
+            echo "Executing operations for Ubuntu..."
             ;;
         armbian)
-            echo "执行针对Armbian的操作..."
+            echo "Executing operations for Armbian..."
             ;;
         *)
-            echo "未知的系统类型: $OS"
+            echo "Unknown system type: $OS"
             ;;
     esac
 else
-    echo "不调正仓库，直接升级,安装基本包"
+    echo "Not adjusting repository, directly upgrading and installing basic packages"
     case $OS in
         debian)
-            # echo "执行针对Debian的操作..."
+            # echo "Executing operations for Debian..."
             # echo 'deb http://ftp.debian.org/debian buster-backports main' | tee /etc/apt/sources.list.d/buster-backports.list
             # apt update -y
             # apt install linux-headers-$(uname -r) -y
@@ -225,13 +225,13 @@ else
             # modprobe wireguard && lsmod | grep wireguard
             ;;
         ubuntu)
-            echo "执行针对Ubuntu的操作..."
+            echo "Executing operations for Ubuntu..."
             ;;
         armbian)
-            echo "执行针对Armbian的操作..."
+            echo "Executing operations for Armbian..."
             ;;
         *)
-            echo "未知的系统类型: $OS"
+            echo "Unknown system type: $OS"
             ;;
     esac
 fi
@@ -240,16 +240,16 @@ chang_wireguard
 #######################################################chang_sysctl########################################################################################
 chang_sysctl() {
 echo ""
-echo "调整系统sysctl信息"
+echo "Adjusting system sysctl information"
 cat > /etc/sysctl.conf << TEMPEOF
-##提高整个系统的文件限制
+## Increase the file limit for the entire system
 fs.file-max = 35525
 net.core.rmem_max = 67108864
 net.core.wmem_max = 67108864
 net.core.netdev_max_backlog = 250000
 net.core.somaxconn = 32400
 fs.fanotify.max_queued_events = 65536
-##调整内核打开数
+## Adjust kernel open file descriptors
 net.ipv4.tcp_syncookies = 1
 net.ipv4.ip_forward=1
 net.ipv4.tcp_tw_reuse = 1
@@ -261,22 +261,22 @@ net.ipv4.tcp_max_syn_backlog = 8192
 #net.ipv4.tcp_max_tw_buckets = 5000
 #net.netfilter.nf_conntrack_max=1048576
 #net.nf_conntrack_max=1048576
-##开启TCP Fast Open
+## Enable TCP Fast Open
 net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
 net.ipv4.tcp_mtu_probing = 1
-##开启bbr
+## Enable bbr
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
-##虚拟内存优化
+## Virtual memory optimization
 vm.swappiness=10
-##关闭IPV6
+## Disable IPV6
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 TEMPEOF
 sysctl -p
-echo "成功调整内核参数，开启BBR"
+echo "Successfully adjusted kernel parameters and enabled BBR"
 }
 chang_sysctl
