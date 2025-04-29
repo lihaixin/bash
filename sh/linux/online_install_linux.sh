af6114e7577b08d81598e2535b7c3b91c503292f
lihaixin <lihaixin@15099.net>
2025-04-29 15:05:12 +0800

    Update online_install_linux.sh

--- sh/linux/online_install_linux.sh
+++ sh/linux/online_install_linux.sh
@@ -6,8 +6,15 @@ prompt="Please enter the installation password, using the default value ( $DEFAU
 read -t 20 -p "$prompt" PASSWORD || PASSWORD=$DEFAULT_VALUE
 : ${PASSWORD:=$DEFAULT_VALUE}
 
+# if [ "$COUNTRY" == "cn" ]; then
+#   bash <(wget --no-check-certificate -qO- 'https://bash.15099.net/linux/netinstallos.sh') -d 12 -v 64 -p "$PASSWORD" --mirror 'http://mirrors.aliyun.com/debian/'
+# else
+#   bash <(wget --no-check-certificate -qO- 'https://bash.15099.net/linux/netinstallos.sh') -d 12 -v 64 -p "$PASSWORD"
+# fi
 if [ "$COUNTRY" == "cn" ]; then
-  bash <(wget --no-check-certificate -qO- 'https://bash.15099.net/linux/netinstallos.sh') -d 12 -v 64 -p "$PASSWORD" --mirror 'http://mirrors.aliyun.com/debian/'
+  curl -O https://cnb.cool/bin456789/reinstall/-/git/raw/main/reinstall.sh || wget -O /tmp/online_install_debian.sh $_ 
 else
-  bash <(wget --no-check-certificate -qO- 'https://bash.15099.net/linux/netinstallos.sh') -d 12 -v 64 -p "$PASSWORD"
+  curl -O https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh || wget -O /tmp/online_install_debian.sh $_
 fi
+bash /tmp/online_install_debian.sh debian12
+rm -rf /tmp/online_install_debian.sh
