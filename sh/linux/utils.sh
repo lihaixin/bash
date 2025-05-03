8e2cf289073e60add7491284b6f25a6dfa0cdeae
lihaixin <lihaixin@15099.net>
2025-05-03 19:39:51 +0800

    Create utils.sh

---
+++ sh/linux/utils.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+export TERM=xterm-256color
+# 定义颜色
+RED='\033[0;31m'    # 错误或警告信息
+GREEN='\033[0;32m'  # 成功信息
+YELLOW='\033[0;33m' # 警告或提示信息
+BLUE='\033[0;34m'   # 一般信息
+CYAN='\033[0;36m'   # 时间戳或强调信息
+NC='\033[0m'        # 重置颜色
+
+# 定义日志文件
+LOG_FILE=${LOG_FILE:-"/tmp/15099.log"}
+
+# 创建日志文件并设置权限（如果需要）
+touch "$LOG_FILE" && chmod 644 "$LOG_FILE"
+
+# 日志输出函数（带时间戳和类别）
+log_message() {
+    local type=$1    # 日志类别：INFO, SUCCESS, WARNING, ERROR
+    local message=$2 # 日志内容
+
+    case $type in
+        INFO)
+            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${BLUE}[INFO]${NC} $message"
+            ;;
+        SUCCESS)
+            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[SUCCESS]${NC} $message"
+            ;;
+        WARNING)
+            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}[WARNING]${NC} $message"
+            ;;
+        ERROR)
+            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}[ERROR]${NC} $message"
+            ;;
+        *)
+            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${NC}$message"
+            ;;
+    esac
+}
+
+# 使用 read_with_message
+read_with_message() {
+    local prompt_message="$1"
+    local variable_name="$2"
+    read -p "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${BLUE}[INFO]${NC}$prompt_message: " -r "$variable_name"
+}
