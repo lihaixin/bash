2fa58d6a9c0c124ca43134c044da91b80fc70303
lihaixin <lihaixin@15099.net>
2025-05-03 20:02:07 +0800

    Update index.sh

--- sh/linux/index.sh
+++ sh/linux/index.sh
@@ -1,43 +1,4 @@
 #!/bin/bash
-
-# 定义颜色
-RED='\033[0;31m'    # 错误或警告信息
-GREEN='\033[0;32m'  # 成功信息
-YELLOW='\033[0;33m' # 警告或提示信息
-BLUE='\033[0;34m'   # 一般信息
-CYAN='\033[0;36m'   # 时间戳或强调信息
-NC='\033[0m'        # 重置颜色
-
-# 日志输出函数（带时间戳和类别）
-log_message() {
-    local type=$1    # 日志类别：INFO, SUCCESS, WARNING, ERROR
-    local message=$2 # 日志内容
-
-    case $type in
-        INFO)
-            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${BLUE}[INFO]${NC} $message"
-            ;;
-        SUCCESS)
-            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${GREEN}[SUCCESS]${NC} $message"
-            ;;
-        WARNING)
-            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${YELLOW}[WARNING]${NC} $message"
-            ;;
-        ERROR)
-            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${RED}[ERROR]${NC} $message"
-            ;;
-        *)
-            echo -e "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${NC}$message"
-            ;;
-    esac
-}
-# 使用 read_with_message
-read_with_message() {
-    local prompt_message="$1"
-    local variable_name="$2"
-    read -p "${CYAN}[$(date '+%Y-%m-%d %H:%M:%S')] ${BLUE}[INFO]${NC}$prompt_message: " -r "$variable_name"
-}
-
 # 主菜单函数
 # 提供用户操作选项
 main_menu() {
