1788302cdff4b78834f60e2023ad1598b7b3712d
lihaixin <lihaixin@15099.net>
2025-06-28 23:30:11 +0800

    Create ui.sh

---
+++ sh/ikuai/ui.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# 赋值ui类型 253 为ui 252 为ui agent  251 为ui edge agent, 其他值，例如0 不运行docker run，这样打包程序小
+docker_val=253
+
+# 如需修改 edge 相关参数，直接改下面这两个变量即可
+EDGE_ID="bf655e5b-fe77-4b4e-875a-c1be66413e14"
+EDGE_KEY="aHR0cHM6Ly91aS4xNTA5OS5uZXQ6ODQ0M3x1aS4xNTA5OS5uZXQ6ODAwMnw0TmhQM3dpbDhDSG5BS2M5ejIvQ1J3RkJISVhhdS80ZWZ4aW1Xc1pYQTV3PXw1NDE"
+
+# 检查名为 vlan 的网络是否已存在
+if ! docker network ls --format '{{.Name}}' | grep -wq vlan; then
+  echo "vlan 网络不存在，正在创建..."
+  docker network create -d bridge \
+    --subnet=172.19.0.0/24 \
+    --gateway=172.19.0.254 \
+    vlan
+else
+  echo "vlan 网络已存在，无需创建。"
+fi
+
+# 创建portainer_data卷
+if ! docker volume ls --format '{{.Name}}' | grep -wq "^portainer_data$"; then
+    echo "portainer_data 不存在，正在创建..."
+    docker volume create portainer_data
+else
+    echo "portainer_data 已存在，无需创建。"
+fi
+
+# 获取 Docker_Volumes 路径 (只为252和251准备)
+if [[ "$docker_val" == "252" || "$docker_val" == "251" ]]; then
+    Docker_Volumes=$(docker volume inspect portainer_data --format '{{.Mountpoint}}' 2>/dev/null | sed 's#/portainer_data/_data$##')
+    [ -z "$Docker_Volumes" ] && Docker_Volumes="/var/lib/docker/volumes"
+fi
+
+# 先停止并删除同名容器，防止冲突
+docker rm -f ui 2>/dev/null || true
+
+if [ "$docker_val" == "253" ]; then
+    # 检查是否有ikuaiui镜像
+    if ! docker image ls --format '{{.Repository}}' | grep -wq "^ikuaiui$"; then
+      echo "本地没有ikuaiui镜像，开始拉取..."
+      docker pull lihaixin/ui:ce-2.19.5 || { echo "拉取失败"; exit 1; }
+      docker tag lihaixin/ui:ce-2.19.5 ikuaiui
+      docker rmi lihaixin/ui:ce-2.19.5
+      echo "已拉取并重命名为ikuaiui"
+    else
+      echo "本地已存在ikuaiui镜像，无需拉取。"
+    fi
+    docker run -d \
+        --net=vlan \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v portainer_data:/data \
+        -e TPORT=8002 \
+        -e COUNTRY=cn \
+        -e PASSWORD=@china1234567 \
+        -e TEMPLATES=https://dockerfile.15099.net/index.json \
+        -e NO=0000210012301280114 \
+        --name ui \
+        --ip=172.19.0.253 \
+        ikuaiui
+elif [ "$docker_val" == "252" ]; then
+    docker run -d \
+        --net=vlan \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v ${Docker_Volumes}:/var/lib/docker/volumes \
+        -v /:/host \
+        --name ui \
+        --ip=172.19.0.252 \
+        portainer/agent:2.21.5
+elif [ "$docker_val" == "251" ]; then
+    docker run -d \
+        --net=host \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v ${Docker_Volumes}:/var/lib/docker/volumes \
+        -v /:/host \
+        -v portainer_data:/data \
+        -e EDGE=1 \
+        -e EDGE_ID="${EDGE_ID}" \
+        -e EDGE_KEY="${EDGE_KEY}" \
+        -e EDGE_INSECURE_POLL=1 \
+        --name ui \
+        portainer/agent:2.21.5
+else
+    echo "/etc/mnt/docker 文件内容不是 253、252、251，未执行任何 docker run"
+    exit 1
+fi
