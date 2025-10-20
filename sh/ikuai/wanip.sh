74421bd1a227ac32b96ae11b1160e740aa4c2f3f
lihaixin <lihaixin@15099.net>
2025-10-20 17:01:50 +0800

    Create wanip.sh

---
+++ sh/ikuai/wanip.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# 1. 删除lan1上的静态IP
+ip addr del 192.168.9.1/24 dev lan1 2>/dev/null
+ip addr del 192.168.1.1/24 dev lan1 2>/dev/null
+
+
+# 2. 用udhcpc后台获取IP
+udhcpc -i lan1 -p /var/run/udhcpc.lan1.pid -s /usr/ikuai/script/utils/udhcpc.sh &
+sleep 5  # 等待几秒让udhcpc分配到IP
+
+# 3. 获取lan1当前IP地址和网关
+
+ip_info=$(ip -4 addr show lan1 | awk '/inet / {print $2; exit}')
+gateway=$(ip route show dev lan1 | awk '/default/ {print $3; exit}')
+
+echo "lan1分配到的IP信息:"
+echo "IP地址和掩码: $ip_info"
+echo "网关: $gateway"
+
+# 4. 杀掉 udhcpc 进程
+pidfile="/var/run/udhcpc.lan1.pid"
+if [ -f "$pidfile" ]; then
+    kill $(cat $pidfile)
+    rm -f $pidfile
+fi
+
+# 6. 解绑LAN1网卡
+/bin/bash /etc/ikcommon /usr/ikuai/script/lan.sh del_band id=1
+brctl show
+
+# 7. 把获取到的IP和掩码、网关绑定到eth0
+if [ -n "$ip_info" ]; then
+    ip addr add $ip_info dev eth0
+    echo "已绑定IP $ip_info 到eth0"
+fi
+if [ -n "$gateway" ]; then
+    ip route add default via $gateway dev eth0
+    echo "已设置eth0网关: $gateway"
+fi
+
+# 8. 输出eth0当前IP和网关
+echo "eth0当前地址信息："
+ip addr show eth0
+echo "eth0路由信息："
+ip route show dev eth0
